453e304f68153bd27556d17f3d651ee15cd83889
Evgenia <janemishkina@yandex.ru>
2023-02-11 23:25:23 +0300

    Открываем папки

---
+++ example019/Program.cs
@@ -0,0 +1,25 @@
+﻿/*string path = "C:/Users/JaneM/OneDrive/Документы/GB/Лекция С# examples/Example001";
+DirectoryInfo di = new DirectoryInfo(path);
+System.Console.WriteLine(di.CreationTime);
+FileInfo[] fi = di.GetFiles();
+for (int i = 0; i < fi.Length; i++)
+{
+    System.Console.WriteLine(fi[i].Name);
+}
+*/
+
+void CatalogInfo(string path, string indent = "")
+{
+ DirectoryInfo catalogs = new DirectoryInfo(path);
+ foreach (var currentCatalog in catalogs.GetDirectories())
+ {
+ Console.WriteLine($"{indent}{currentCatalog.Name}");
+ CatalogInfo(currentCatalog.FullName, indent + " ");
+ }
+ foreach (var item in catalogs.GetFiles())
+ {
+ Console.WriteLine($"{indent}{item.Name}");
+ }
+}
+string path = @"C:/Users/JaneM/OneDrive/Документы/GB/Лекция С# examples/Example001";
+CatalogInfo(path);
